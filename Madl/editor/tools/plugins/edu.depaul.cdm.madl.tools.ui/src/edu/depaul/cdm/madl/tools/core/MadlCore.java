8a42734199256f90ab55fc12667844540ea56983
Wei <harryxiaxia@gmail.com>
2013-11-23 14:23:19 -0600

    find madl.tool back

---
+++ Madl/editor/tools/plugins/edu.depaul.cdm.madl.tools.ui/src/edu/depaul/cdm/madl/tools/core/MadlCore.java
@@ -0,0 +1,45 @@
+package edu.depaul.cdm.madl.tools.core;
+
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.Plugin;
+
+/**
+ * @author sstalin
+ *
+ */
+public class MadlCore extends Plugin{
+	  /**
+	   * The unique instance of this class.
+	   */
+	  private static MadlCore PLUG_IN;
+	  /**
+	   * The log used by {@link #logError(String)} and other local methods for logging errors, warnings,
+	   * and information or <code>null</code> to use the default system log.
+	   */
+	  private static ILog PLUGIN_LOG;
+
+	  /**
+	   * Flag indicating whether instrumentation logging of errors is enabled
+	   */
+	  private static boolean instrumentationLogErrorEnabled = true;
+
+	  /**
+	   * The id of the plug-in that defines the Dart model.
+	   */
+	  public static final String PLUGIN_ID = MadlCore.class.getPackage().getName();
+	  
+	  /**
+	   * Return the unique instance of this class.
+	   * 
+	   * @return the unique instance of this class
+	   */
+	  public static MadlCore getPlugin() {
+	    return PLUG_IN;
+	  }
+	  /**
+	   * Initialize a newly created instance of this class.
+	   */
+	  public MadlCore() {
+	    PLUG_IN = this;
+	  }
+}
