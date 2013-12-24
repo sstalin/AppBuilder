5145cb3f7d746f3d82123f5a4b1d7ab064ccd424
Ilya Bernshteyn <ilyabe@gmail.com>
2013-12-23 22:25:42 -0600

    Copying images, hardcoded, will look for a better way to do this

--- Madl/editor/tools/plugins/edu.depaul.cdm.madl.eclipse.ui/src/edu/depaul/madl/wizards/AppBuilderNewJavaProject.java
+++ Madl/editor/tools/plugins/edu.depaul.cdm.madl.eclipse.ui/src/edu/depaul/madl/wizards/AppBuilderNewJavaProject.java
@@ -2,8 +2,15 @@ package edu.depaul.madl.wizards;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileInfo;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.filesystem.IFileSystem;
+import org.eclipse.core.filesystem.IFileTree;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
@@ -12,19 +19,22 @@ import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.ui.ide.FileStoreEditorInput;
 
 import edu.depaul.madl.wizards.constants.ProjectFilenames;
 
 
-public class AppBuilderNewJavaProject extends WizardNewProjectCreationPage{
+public class AppBuilderNewJavaProject extends WizardNewProjectCreationPage {
 	
 	private String pageName = "New AppBuilder Madl Project";
+	IProgressMonitor progressMonitor = null;
 	
 	public AppBuilderNewJavaProject(){
 		super("AppBuilder New Project Page");
@@ -125,21 +135,50 @@ public class AppBuilderNewJavaProject extends WizardNewProjectCreationPage{
 	}
 	
 	private void copyImagesIntoConf(IProject project) {		
-		URL url;
+		URL checkboxOffUrl;
+		URL checkboxOnUrl;
+		URL iconUrl;
+		URL icon2xUrl;
+		URL radioButtonOffUrl;
+		URL radioButtonOnUrl;
 		
-		// Create the empty file
-		IFile file = getProjectHandle().getFile("conf/images/checkbox-off.png");
+		// Create the empty files
+		IFile checkboxOff 		= getProjectHandle().getFile("conf/images/checkbox-off.png");
+		IFile checkboxOn 		= getProjectHandle().getFile("conf/images/checkbox-on.png");
+		IFile icon 				= getProjectHandle().getFile("conf/images/icon.png");
+		IFile icon2x 			= getProjectHandle().getFile("conf/images/icon@2x.png");
+		IFile radioButtonOff 	= getProjectHandle().getFile("conf/images/radiobutton-off.png");
+		IFile radioButtonOn 	= getProjectHandle().getFile("conf/images/radiobutton-on.png");
 		
 		try {
-			// Get the input from the resources file
-		    url = new URL("platform:/plugin/edu.depaul.cdm.madl.eclipse.ui.madlprojectwizard/resources/images/checkbox-off.png");
-		    InputStream inputStream = url.openConnection().getInputStream();
+			// Get the inputs from the resources files
+			checkboxOffUrl = new URL("platform:/plugin/edu.depaul.cdm.madl.eclipse.ui.madlprojectwizard/resources/images/checkbox-off.png");
+			checkboxOnUrl = new URL("platform:/plugin/edu.depaul.cdm.madl.eclipse.ui.madlprojectwizard/resources/images/checkbox-on.png");
+			iconUrl = new URL("platform:/plugin/edu.depaul.cdm.madl.eclipse.ui.madlprojectwizard/resources/images/icon.png");
+			icon2xUrl = new URL("platform:/plugin/edu.depaul.cdm.madl.eclipse.ui.madlprojectwizard/resources/images/icon@2x.png");
+			radioButtonOffUrl = new URL("platform:/plugin/edu.depaul.cdm.madl.eclipse.ui.madlprojectwizard/resources/images/radiobutton-off.png");
+			radioButtonOnUrl = new URL("platform:/plugin/edu.depaul.cdm.madl.eclipse.ui.madlprojectwizard/resources/images/radiobutton-on.png");
+			
+			InputStream checkboxOffInput = checkboxOffUrl.openConnection().getInputStream();
+			InputStream checkboxOnInput = checkboxOnUrl.openConnection().getInputStream();
+			InputStream iconUrlInput = iconUrl.openConnection().getInputStream();
+			InputStream icon2xUrlInput = icon2xUrl.openConnection().getInputStream();
+			InputStream radioButtonOffUrlInput = radioButtonOffUrl.openConnection().getInputStream();
+			InputStream radioButtonOnUrlInput = radioButtonOnUrl.openConnection().getInputStream();
 		    		    		    
-		    file.create(inputStream, true, null);
-		 
+			// Do the copying
+			checkboxOff.create(checkboxOffInput, true, null);
+			checkboxOn.create(checkboxOnInput, true, null);
+			icon.create(iconUrlInput, true, null);
+			icon2x.create(icon2xUrlInput, true, null);
+			radioButtonOff.create(radioButtonOffUrlInput, true, null);
+			radioButtonOn.create(radioButtonOnUrlInput, true, null);	    
+		    
 		} catch (IOException e) {
+			System.err.println("copyImagesIntoConf error");
 		    e.printStackTrace();
 		} catch (CoreException e) {
+			System.err.println("copyImagesIntoConf error");
 			e.printStackTrace();
 		}
 	}
