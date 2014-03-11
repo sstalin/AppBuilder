5e554c5668eb63cd13a107aafb74e256cce509d9
Wei <harryxiaxia@gmail.com>
2014-03-10 19:42:38 -0500

    Customize for launcer

--- Madl/editor/tools/plugins/edu.depaul.cdm.madl.launcher/src/edu/depaul/cdm/madl/launch/MadlLaunch.java
+++ Madl/editor/tools/plugins/edu.depaul.cdm.madl.launcher/src/edu/depaul/cdm/madl/launch/MadlLaunch.java
@@ -6,6 +6,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.debug.core.model.ISourceLocator;
 import org.eclipse.jdt.launching.AbstractJavaLaunchConfigurationDelegate;
 import org.eclipse.jdt.launching.JavaLaunchDelegate;
 
@@ -15,14 +16,21 @@ import edu.depaul.madl.wizards.AppBuilderConfiguration;
 public class MadlLaunch extends AbstractJavaLaunchConfigurationDelegate implements ILaunchConfigurationDelegate {
 
 //	AppBuilderConfiguration config;
-
+	
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode,
 			ILaunch launch, IProgressMonitor monitor) throws CoreException {
 		// TODO Auto-generated method stub
+		ISourceLocator locator = launch.getSourceLocator();
 		JavaLaunchDelegate javaLauncher = new JavaLaunchDelegate();
+		
+		//launch
+		String attr = launch.getAttribute(configuration.toString());
+		
 		javaLauncher.launch(configuration, mode, launch, monitor);
-		//AppBuilderConfiguration.getInstance().getProject().getProject().refreshLocal(IResource.DEPTH_INFINITE, null);
+//		javaLauncher.launch(configuration, mode, launch, null);
+		
+//		AppBuilderConfiguration.getInstance().getProject().getProject().refreshLocal(IResource.DEPTH_INFINITE, null);
 	}
 
 }
